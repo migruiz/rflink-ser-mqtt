abbca62847b4164d5539d9f07b71384e7661d147
migruiz <mig.ruiz@gmail.com>
2019-08-22 16:39:58 +0100

    creating json object

--- App/serial.js
+++ App/serial.js
@@ -1,13 +1,30 @@
-const raspi = require('raspi');
-const Serial = require('raspi-serial').Serial;
-
-global.mtqqLocalPath = process.env.MQTTLOCAL;
-
-raspi.init(() => {
-  var serial = new Serial({portId:'/dev/ttyUSB0', baudRate:57600});
-  serial.open(() => {
-    serial.on('data', (data) => {
-      process.stdout.write(data);
-    });
-  });
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+var port = new SerialPort("/dev/ttyUSB0", {
+  baudRate: 57600,
 });
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', process)
+
+function process(data){
+  data = someText = data.replace(/(\r\n|\n|\r)/gm,"");
+  var segments = data.toString().split(";")
+  if (segments[0]!=="20"){
+    return;
+  }
+  if (segments[0]==="00"){
+    return;
+  }
+  const deviceType = segments[2];
+  var dataObject ={deviceType:deviceType}  
+  for (let index = 3; index < segments.length; index++) {
+    const labelElement = segments[index];
+    var subSegments = labelElement.split("=");
+    const key = subSegments[0];
+    const value = subSegments[1]
+    if (value){
+      dataObject[key] = value
+    }
+  }
+  console.log(JSON.stringify(dataObject));
+}
